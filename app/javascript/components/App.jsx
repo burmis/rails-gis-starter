87b225838568198268bfe7654810a073686c7d34
Mark Bennett <mark@burmis.ca>
2023-01-28 00:08:15 +0000

    Display the map coordinates

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -6,10 +6,11 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibWFya2Jlbm5ldHQiLCJhIjoiY2tyamVlcTdhMTRiYjJvb
 export default props => {
     const mapContainer = useRef(null);
     const map = useRef(null);
-    const [lng, setLng] = useState(-70.9);
-    const [lat, setLat] = useState(42.35);
+    const [lng, setLng] = useState(-113.4816);
+    const [lat, setLat] = useState(53.5294);
     const [zoom, setZoom] = useState(9);
 
+    // Centre the map on Edmonton on load
     useEffect(() => {
         if (map.current) return; // initialize map only once
         map.current = new mapboxgl.Map({
@@ -20,8 +21,21 @@ export default props => {
         });
     });
 
+    // Update the coordinates and zoom on move
+    useEffect(() => {
+        if (!map.current) return; // wait for map to initialize
+        map.current.on('move', () => {
+            setLng(map.current.getCenter().lng.toFixed(4));
+            setLat(map.current.getCenter().lat.toFixed(4));
+            setZoom(map.current.getZoom().toFixed(2));
+        });
+    });
+
     return <div>
         <h1>Our React App</h1>
+        <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+        </div>
         <div ref={mapContainer} className="map-container" />
     </div>;
 }
